f4c258018687e4708bfb7ddc64137bea64936432
Chloe Unrau <chloe.unrau@gmail.com>
2014-02-14 11:49:30 -0800

    Change mouseover buttons to single images

--- header.php
+++ header.php
@@ -13,23 +13,6 @@
 <body> <!--ends in footer.php -->
 <div id="wrapper"> <!-- ends in footer.php -->
 
-<div id="image-loader">
-  <img src="<?php echo bloginfo('template_url'); ?>/images/menu-blog-s.png">
-  <img src="<?php echo bloginfo('template_url'); ?>/images/menu-home-s.png">
-  <img src="<?php echo bloginfo('template_url'); ?>/images/menu-portfolio-s.png">
-
-  <img src="<?php echo bloginfo('template_url'); ?>/images/social-email-s.png">
-  <img src="<?php echo bloginfo('template_url'); ?>/images/social-twitter-s.png">
-  <img src="<?php echo bloginfo('template_url'); ?>/images/social-github-s.png">
-  <img src="<?php echo bloginfo('template_url'); ?>/images/social-linkedin-s.png">
-  <img src="<?php echo bloginfo('template_url'); ?>/images/social-resume-s.png">
-
-  <img src="<?php echo bloginfo('template_url'); ?>/images/small-email-s.png">
-  <img src="<?php echo bloginfo('template_url'); ?>/images/small-github-s.png">
-  <img src="<?php echo bloginfo('template_url'); ?>/images/small-linkedin-s.png">
-  <img src="<?php echo bloginfo('template_url'); ?>/images/small-twitter-s.png">
-</div>
-
 <header>
   <a href="<?php echo bloginfo('home'); ?>">
     <img src="<?php echo bloginfo('template_url'); ?>/images/header.png" width="375px" height="72px">
